f924580b922fe5afbf3180a634116c4a0b7120c0
Andreas Meier <andreas.meier@bbv.ch>
2017-10-09 20:19:18 +0200

    amp-showcase: pru-farbsort: fix pru firmware location

--- target/chroot/beagleboard.org-jessie.sh
+++ target/chroot/beagleboard.org-jessie.sh
@@ -329,7 +329,7 @@ install_git_repos () {
 
        cd ${git_target_dir}/
        make --makefile=Makefile.pru
-       cp ./gen/pru-farbsort.out /lib/firmware/am335x-pru0-fw
+       cp ./bin/am335x-pru0-fw  /lib/firmware/
        update-initramfs -u -k ${repo_rcnee_pkg_version}
 
        git_repo="https://github.com/bbvch/farbsort-websocket"
